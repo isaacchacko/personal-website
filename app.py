67585cfdb06e7d80780d20e25b4f92d171de5a97
isaacchacko <isaac.chacko05@gmail.com>
2023-06-20 18:19:50 -0500

    updated website

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from flask import Flask
 from views import views
 
-# https://www.youtube.com/watch?v=kng-mJJby8g&ab_channel=TechWithTim
-
 app = Flask(__name__)
 app.register_blueprint(views, url_prefix='/')
 
